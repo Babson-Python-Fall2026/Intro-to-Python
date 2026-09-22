4df6da2c7695db0cabad581856934cc76b4b5474
Robert Slotpole <rslotpole@babson.edu>
2026-09-22 16:33:26 -0400

    feat: create shared_references.py

---
+++ sandbox/shared_references.py
@@ -0,0 +1,24 @@
+a = '12'
+b = a * 3
+
+print(b)
+for l in b:
+    print (id(l))  # all '1's and '2's have same id
+print()
+
+c = [1,2]
+d = c * 3
+print(c,d)
+print(id(c),id(d)) # c and d different objects, d is one big list
+print()
+
+e = [c] * 3  
+print(e)      # e is a list of lists
+for item in e:
+    print(id(item))  #!!! notice that each inner list has the same id.
+
+print()
+e[0].append(99)
+print(e)
+
+# how can we create a list of lists so each inner list is a different object with unique id
